d0052481e055c9c0a269ff2e62d5ba22f762e5d9
zhangyixuan <18203399109@163.com>
2018-05-29 16:46:00 +0800

    添加搜索框

--- js/footer.js
+++ js/footer.js
@@ -7,15 +7,17 @@ $(function(){
 $(".dh li a").on('click',function(){
 	// console.log($(this).parent().html());
 	var _this = $(this).parent();//columnIndex
-	//console.log(_this.html());
+	$(".navsearch").attr("style","display: none;");
+	$(".navlist").attr("style","display: block;");
 	switch(_this.index()){
 		case 0:
-			index1();
+			index0();
 			break;
 		case 1:
-			index2();
+			index1();
 			break;
 		case 2:
+			index2();
 			break;
 		case 3:
 			index3();
@@ -28,17 +30,21 @@ $(".dh li a").on('click',function(){
 			break;
 	}
 });
-function index1(){
+function index0(){
 	var htm = "<li class='navli'><a href='javascript:void(0)'>A全部科目</a></li>"+
 				"<li class='navli'><a href='javascript:void(0)'>A会计从业</a></li>";
 	$(".clearfix").html(htm);
 	$('.wrapper').navbarscroll();
 }
-function index2(){
+function index1(){
 	var htm = "<li class='navli'><a href='javascript:void(0)'>B全部科目</a></li>"+
 				"<li class='navli'><a href='javascript:void(0)'>B会计从业</a></li>";
 	$(".clearfix").html(htm);
 	$('.wrapper').navbarscroll();
+}
+function index2(){
+	$(".navlist").attr("style","display: none;");
+	$(".navsearch").attr("style","display: block;");
 }
 function index3(){
 	$(".clearfix").html("");
